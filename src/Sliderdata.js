ce1e8b13e98aa81b620d4067d9ff7e8a7475c521
john <johnadexzy@gmail.com>
2022-01-29 04:21:32 +0100

    slider redesigned

---
+++ src/Sliderdata.js
@@ -0,0 +1,21 @@
+import slide1 from './img/greensofa.png';
+
+const Dataslider = [
+  {
+    title: 'ELEGANT SOFAS',
+    desc: 'We Design Pieces Of Funiture And objects The Perfectly Bridge The Gap Between Functionality And Beauty',
+    img: { slide1 },
+  },
+  {
+    title: 'Beautiful Lightings',
+    desc: 'We Design Pieces Of Funiture And objects The Perfectly Bridge The Gap Between Functionality And Beauty',
+    img: { slide1 },
+  },
+  {
+    title: 'kitchen furniture',
+    desc: 'We Design Pieces Of Funiture And objects The Perfectly Bridge The Gap Between Functionality And Beauty',
+    img: { slide1 },
+  },
+];
+
+export default Dataslider;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,42 +1,132 @@
 import styled from 'styled-components';
-import slide1 from '../img/greensofa.png';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { useState } from 'react';
+import Dataslider from '../Sliderdata';
 
-const Container = styled.div`
-  background-color: red;
-`;
 const Slidercontainer = styled.div`
-  width: 100vw;
-  height: 50rem;
+  width: 100%;
+  position: relative;
+  display: flex;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.sliderIndex * -100}vw);
+  background-color: #fff;
+`;
+const Slidercontrol = styled.div`
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3rem;
+  width: 3rem;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === 'left' && '10px'};
+  right: ${(props) => props.direction === 'right' && '10px'};
+  cursor: pointer;
+  margin: auto;
+  position: absolute;
+  border-radius: 50%;
+  z-index: 2;
+`;
+
+const Slide = styled.div`
+  width: 100%;
+  height: 40rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
 `;
 const Image = styled.img`
-  width: 100%;
+  width: 100vw;
   height: 100%;
+  object-fit: cover;
 `;
 const Infocontainer = styled.div`
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+
+  height: 100%;
+  width: 100%;
+  padding-bottom: 7rem;
+  color: #fff;
 `;
-const Title = styled.div``;
-const Desc = styled.div``;
-const Btn = styled.button``;
+const Title = styled.h1`
+  font-size: 3rem;
+  text-transform: uppercase;
+  letter-spacing: 1.3rem;
+  padding-bottom: 1rem;
+`;
+const Desc = styled.p`
+  font-size: 1.1rem;
+  padding-bottom: 1.5rem;
+`;
+const Btn = styled.button`
+  border: none;
+  padding: 1.3rem;
+  width: 12rem;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 600;
+  border-radius: 1rem;
+  background-color: #ffd60a;
+  transition: 0.2s ease all;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: #ffd60ab1;
+    transform: scale(1.1);
+  }
+`;
 const Slider = () => {
+  const [sliderIndex, setSliderIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      if (sliderIndex !== Dataslider.length - 1) {
+        setSliderIndex(sliderIndex + 1);
+      } else if (sliderIndex === Dataslider.length - 1) {
+        setSliderIndex(0);
+      }
+    } else if (direction === 'right') {
+      if (sliderIndex !== 0) {
+        setSliderIndex(sliderIndex - 1);
+      } else if (sliderIndex === 0) {
+        setSliderIndex(Dataslider.length - 1);
+      }
+    }
+  };
   return (
-    <Container>
-      <Slidercontainer>
-        <Image src={slide1} />
-        <Infocontainer>
-          <Title>Elegant Sofas</Title>
-          <Desc>
-            We Design Pieces Of Funiture And objects The Perfectly Bridge The
-            Gap Between Functionality And Beauty
-          </Desc>
-          <Btn>Shop Now</Btn>
-        </Infocontainer>
-      </Slidercontainer>
-    </Container>
+    <Slidercontainer>
+      <Slidercontrol direction="left" onClick={() => handleClick('left')}>
+        <ArrowLeftIcon />
+      </Slidercontrol>
+      <Wrapper sliderIndex={sliderIndex}>
+        {Dataslider.map((data, index) => {
+          return (
+            <Slide key={index}>
+              <Image src={data.img} />
+              <Infocontainer>
+                <Title>{data.title}</Title>
+                <Desc>{data.desc}</Desc>
+                <Btn>Shop Now</Btn>
+              </Infocontainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
+      <Slidercontrol direction="right" onClick={() => handleClick('right')}>
+        <ArrowRightIcon />
+      </Slidercontrol>
+    </Slidercontainer>
   );
 };
 
